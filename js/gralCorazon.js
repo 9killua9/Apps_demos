5ee688d891013ce8273fd7acae6c5b9b124cc27a
Leandro Salar <info@reiatsu.com.ar>
2013-08-06 01:53:24 -0300

    ajajaj !

--- js/gralCorazon.js
+++ js/gralCorazon.js
@@ -8,8 +8,9 @@ $w = parseInt($(window).width());
 $(document).on('ready', function(){
     $("#logu").css({marginTop:($h/2)-35, marginLeft:($w/2)-196});
     $(".w100").css("width",$w-180);
-    $("input").width($w-12);
+    $("input").width($w-92);
     $(".conteBotVerMas").width($w-32);
+    $("#topCorazon").css({marginTop:($h/2)-210, marginLeft:($w/2)-130})
 });
 
 function cargaAlgoIndex()
@@ -74,12 +75,13 @@ function cargaAlgo()
                     },"3000"); } else alert("Complete los nombres para saber tu coincidencia."); } }); $t = setTimeout(function(){corazonBlink(); },1000); 
 }
 
-function corazonBlink(q) {if(q == "otro") {$('#corazonito').destroy(); $('#corazonito').removeClass("muyTriste").removeClass("muyFeliz").removeClass("feliz").removeClass("epa").removeClass("triste").addClass("blink"); } $('#corazonito').sprite({fps: 9, no_of_frames: 22, on_first_frame: function(obj) {obj.spState(1); }, on_last_frame: function(obj) {obj.spStop(); $t = setTimeout(function(){if(q == "triste") corazonTriste(); else obj.spStart(); },4000); } }); clearTimeout($t); } function corazonEpa()
+function corazonBlink(q) {if(q == "otro") {$('#corazonito').destroy(); $('#corazonito').removeClass("muyTriste").removeClass("muyFeliz").removeClass("feliz").removeClass("epa").removeClass("triste").addClass("blink"); } $('#corazonito').sprite({fps: 9, no_of_frames: 22, on_first_frame: function(obj) {obj.spState(1); }, on_last_frame: function(obj) {obj.spStop(); $t = setTimeout(function(){if(q == "triste") corazonTriste(); else obj.spStart(); },4000); } }); clearTimeout($t); } 
+function corazonEpa()
 {
     $('#corazonito').destroy();
     $('#corazonito').removeClass("muyTriste").removeClass("muyFeliz").removeClass("feliz").removeClass("blink").removeClass("triste").addClass("epa");
     $('#corazonito').sprite({
-                            fps: 99, 
+                            fps: 9, 
                             no_of_frames: 32, 
                             on_first_frame: function(obj) {
                                 obj.spState(1); 
